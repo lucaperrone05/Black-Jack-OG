f08bc03df558ff9d98a3cb95d06ff67b65716d34
OnofrioSoranno <sorannoonofrio@outlook.it>
2025-06-05 12:53:18 +0200

    Inizio menu gioco

--- main.c
+++ main.c
@@ -48,27 +48,55 @@ int main() {
             printf("\nOpzione non valida!\n");
             Sleep(1500);
             system("cls"); // Pulisce lo schermo
+			break;
         }
     } while (exit==0 && utente.loggato==0);
 
 
-	exit = 0; // reset exit per il gioco
-	menu = 0; // reset menu per il gioco
-
     if (utente.loggato) {
 
-        do {
-            system("cls");
-            printCentered("Benvenuto nel gioco di BlackJack!\n\n");
-            printf("Nome utente: %s\n", utente.username);
-            printf("Saldo: %d\n", utente.saldo);
-            printf("\n\n");
+       
+        system("cls");
+        printCentered("Benvenuto nel gioco di BlackJack!\n\n");
+        printf("Nome utente: %s\n", utente.username);
+        printf("Saldo: %d\n", utente.saldo);
+        printf("\n\n");
 
+        printCentered("Selezionare un'opzione:\n\n");
+        printCentered("1. Nuova partita\n");
+        printCentered("2. Carica partita\n");
+        printCentered("3. Visualizza storico partite\n");
+        printCentered("4. Exit\n");
+    
+        exit = 0; // reset exit per il gioco
+        menu = 0; // reset menu per il gioco
+
+        do {
             getScelta(&menu);
 
 
             switch (menu) {
+                case 1:
+                    
+					break;
+                case 2:
 
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    system("cls");
+                    printf("\n\n\n");
+                    printCentered("Chiusura del programma...");
+                    printf("\n\n\n");
+                    exit = 1;
+                    break;
+                defualt:
+                    printf("\nOpzione non valida!\n");
+                    Sleep(1500);
+					system("cls"); // Pulisce lo schermo
+					break;
             }
         }while(exit == 0);
         
